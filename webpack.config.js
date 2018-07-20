333da042106e3409c775b1920e24500368e96a63
masashi <frantic.cranberry495@gmail.com>
2018-07-20 21:43:26 +0900

    初回コミット

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "public/",
+    historyApiFallback: true,
+    port: 8080
+  }
+}
